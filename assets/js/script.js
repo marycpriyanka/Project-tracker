a99064356cc7d6733a370a72b14cee3d83fa6c4e
marycpriyanka <marycpriyanka@gmail.com>
2022-09-08 11:38:21 -0700

    Add modal with form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
-$(function() {
+$(function () {
     let dateElement = $("#date");
-    let timer = setInterval(function() {
+    let timer = setInterval(function () {
         dateElement.text(moment().format("MMM DD YYYY, h:mm:ss a"));
     }, 1000);
-    
+
+    // Datepicker widget with minimum date setting
+    $("#datepicker").datepicker({ minDate: new Date(moment()) });
 });
