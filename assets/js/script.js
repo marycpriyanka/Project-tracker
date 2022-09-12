8708fc93087316b6375a3df1d44d6ddc3091e427
marycpriyanka <marycpriyanka@gmail.com>
2022-09-12 13:49:33 -0700

    Capture form data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
+// References to DOM elements
+const projectFormEl = $("#projectForm");
+const projectNameEl = $("#projectName");
+const projectTypeEl = $("#projectType");
+const hourlyWageEl = $("#hourlyWage");
+const datepickerEl = $("#datepicker");
+
+// Submit event listener to the form element
+projectFormEl.on("submit", handleFormSubmit);
+
 $(function () {
     let dateElement = $("#date");
     let timer = setInterval(function () {
@@ -5,5 +15,26 @@ $(function () {
     }, 1000);
 
     // Datepicker widget with minimum date setting
-    $("#datepicker").datepicker({ minDate: new Date(moment()) });
+    datepickerEl.datepicker({ minDate: new Date(moment()) });
 });
+
+// Submit event listener to the form element
+function handleFormSubmit(event) {
+    event.preventDefault();
+
+    // Capture input values from the form
+    const projectName = projectNameEl.val().trim();
+    const projectType = projectTypeEl.val().trim();
+    const hourlyWage = hourlyWageEl.val().trim();
+    const datepicker = datepickerEl.val().trim();
+
+    // Pass the captured data to printProjectData function
+    printProjectData(projectName, projectType, hourlyWage, datepicker);
+}
+
+// Handle printing project data
+function printProjectData(projectName, projectType, hourlyWage, datepicker) {
+    
+}
+
+
